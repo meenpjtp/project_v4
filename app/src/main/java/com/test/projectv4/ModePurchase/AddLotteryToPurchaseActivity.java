0fd4708502fc23f2f41a9bacefc53e7ad7257488
5710451398 <pinjuta.t@ku.th>
2018-03-18 00:53:34 +0700

    complete purchase mode

--- app/src/main/java/com/test/projectv4/ModePurchase/AddLotteryToPurchaseActivity.java
+++ app/src/main/java/com/test/projectv4/ModePurchase/AddLotteryToPurchaseActivity.java
@@ -1,15 +1,183 @@
 package com.test.projectv4.ModePurchase;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.test.projectv4.CustomListView.CustomListViewPurchase;
+import com.test.projectv4.DatabaseHelper.DBHelperPurchase;
+import com.test.projectv4.DatabaseHelper.DBHelperSeenPrize;
+import com.test.projectv4.Model.PurchaseModel;
 import com.test.projectv4.R;
+import com.test.projectv4.Validation.InputValidation;
+
+import java.util.ArrayList;
 
 public class AddLotteryToPurchaseActivity extends AppCompatActivity {
+
+    private TextView tvPurchasePrice;
+    private TextView tvPurchaseSelectedDate;
+    private Spinner spPurchaseSelectDate;
+    private TextInputLayout tilPurchaseAddLottery;
+    private TextInputLayout tilPurchaseAddAmountLottery;
+    private TextInputEditText etPurchaseAddLottery;
+    private TextInputEditText etPurchaseAddAmountLottery;
+    private Button btPurchaseSave;
+
+    private final int PRICE_LOTTERY = 80;
+
+    private String[] myString;
+    private String Selecteditem;
+
+    private ArrayList<PurchaseModel> models;
+    private CustomListViewPurchase customListView;
+
+    private int ID = -1;
+
+    private DBHelperPurchase purchaseHelper;
+    private DBHelperSeenPrize seenPrizeHelper;
+
+    private InputValidation inputValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lottery_to_purchase);
+
+        /**
+         *
+         * Initialize
+         *
+         */
+
+        tvPurchasePrice = (TextView) findViewById(R.id.textViewPriceLotteryPurchase);
+        tvPurchaseSelectedDate = (TextView) findViewById(R.id.textViewSelectedDateLotteryPurchase);
+        spPurchaseSelectDate = (Spinner) findViewById(R.id.spinnerSelectDatePurchase);
+        tilPurchaseAddAmountLottery = (TextInputLayout) findViewById(R.id.textInputLayoutAddAmountLotteryPurchase);
+        tilPurchaseAddLottery = (TextInputLayout) findViewById(R.id.textInputLayoutAddLotteryNumberPurchase);
+        etPurchaseAddAmountLottery = (TextInputEditText) findViewById(R.id.editTextAddAmountLotteryPurchase);
+        etPurchaseAddLottery = (TextInputEditText) findViewById(R.id.editTextAddLotteryNumberPurchase);
+        btPurchaseSave = (Button) findViewById(R.id.buttonSavePurchase);
+
+        purchaseHelper = new DBHelperPurchase(this);
+        seenPrizeHelper = new DBHelperSeenPrize(this);
+
+        inputValidation = new InputValidation(this);
+
+        /**
+         *
+         * Spinner
+         *
+         */
+        ArrayAdapter adapter;
+        Resources res = getResources();
+        myString = res.getStringArray(R.array.lotteries);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, myString);
+        spPurchaseSelectDate.setAdapter(adapter);
+        tvPurchasePrice.setText(String.valueOf(PRICE_LOTTERY));
+
+        spPurchaseSelectDate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Selecteditem = spPurchaseSelectDate.getSelectedItem().toString();
+                tvPurchaseSelectedDate.setText(Selecteditem);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                
+            }
+        });
+
+        /**
+         *
+         * Button Save
+         *
+         */
+
+        btPurchaseSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Fields is empty!
+                if (!inputValidation.isInputEditTextLottery(etPurchaseAddAmountLottery,
+                        tilPurchaseAddAmountLottery, getString(R.string.input_every_fields))) {
+                    return;
+                }
+                if(!inputValidation.isInputEditTextLottery(etPurchaseAddLottery,
+                        tilPurchaseAddLottery, getString(R.string.input_every_fields))){
+                    return;
+                }
+
+
+
+                //Input Complete
+                if(seenPrizeHelper.lotteryIsEmpty(spPurchaseSelectDate.getSelectedItem().toString().trim())){
+
+                    if(seenPrizeHelper.checkLottery(spPurchaseSelectDate.getSelectedItem().toString().trim(),
+                            etPurchaseAddLottery.getText().toString().trim())) {
+                        Toast.makeText(AddLotteryToPurchaseActivity.this, R.string.save_complete, Toast.LENGTH_SHORT).show();
+
+
+                        //Save to database
+                        purchaseHelper.addLottery(new PurchaseModel(ID,
+                                spPurchaseSelectDate.getSelectedItem().toString(),
+                                etPurchaseAddLottery.getText().toString().trim(),
+                                etPurchaseAddAmountLottery.getText().toString(),
+                                String.valueOf(Integer.parseInt(etPurchaseAddAmountLottery.getText().toString())*PRICE_LOTTERY),
+                                getString(R.string.win_lotto)));
+                        clear();
+                        Intent intent = new Intent(AddLotteryToPurchaseActivity.this, ModePurchaseActivity.class);
+                        startActivity(intent);
+                    } else {
+
+                        Toast.makeText(AddLotteryToPurchaseActivity.this, R.string.save_complete, Toast.LENGTH_SHORT).show();
+
+                        //Save to database
+                        purchaseHelper.addLottery(new PurchaseModel(ID,
+                                spPurchaseSelectDate.getSelectedItem().toString(),
+                                etPurchaseAddLottery.getText().toString().trim(),
+                                etPurchaseAddAmountLottery.getText().toString(),
+                                String.valueOf(Integer.parseInt(etPurchaseAddAmountLottery.getText().toString())*PRICE_LOTTERY),
+                                getString(R.string.lose_lotto)));
+                        clear();
+                        Intent intent = new Intent(AddLotteryToPurchaseActivity.this, ModePurchaseActivity.class);
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(AddLotteryToPurchaseActivity.this, R.string.save_complete, Toast.LENGTH_SHORT).show();
+
+                    //Save to database
+                    purchaseHelper.addLottery(new PurchaseModel(ID,
+                            spPurchaseSelectDate.getSelectedItem().toString(),
+                            etPurchaseAddLottery.getText().toString().trim(),
+                            etPurchaseAddAmountLottery.getText().toString(),
+                            String.valueOf(Integer.parseInt(etPurchaseAddAmountLottery.getText().toString())*PRICE_LOTTERY),
+                            getString(R.string.wait_lotto)));
+                    clear();
+                    Intent intent = new Intent(AddLotteryToPurchaseActivity.this, ModePurchaseActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
+
+    private void clear(){
+        etPurchaseAddAmountLottery.setText(null);
+        etPurchaseAddLottery.setText(null);
     }
 }
--- app/src/main/java/com/test/projectv4/ModePurchase/ModePurchaseActivity.java
+++ app/src/main/java/com/test/projectv4/ModePurchase/ModePurchaseActivity.java
@@ -1,15 +1,150 @@
 package com.test.projectv4.ModePurchase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+import com.test.projectv4.CustomListView.CustomListViewPurchase;
+import com.test.projectv4.DatabaseHelper.DBHelperPurchase;
+import com.test.projectv4.Model.PurchaseModel;
 import com.test.projectv4.R;
 
+import java.util.ArrayList;
+
 public class ModePurchaseActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFloatingAddlotteryPurchase;
+
+    private DBHelperPurchase purchaseHelper;
+    private ArrayList<PurchaseModel> models;
+    private CustomListViewPurchase customListView;
+
+    private SwipeMenuListView mSwipPurchase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_purchase);
+
+        mSwipPurchase = (SwipeMenuListView) findViewById(R.id.purchaseListView);
+        mFloatingAddlotteryPurchase = (FloatingActionButton) findViewById(R.id.fabAddlotteryPurchase);
+
+        purchaseHelper = new DBHelperPurchase(this);
+        querySQL();
+
+        /**
+         *
+         *  Floating Action Button
+         *
+         */
+        mFloatingAddlotteryPurchase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addLotteryPurchase = new Intent(ModePurchaseActivity.this, AddLotteryToPurchaseActivity.class);
+                startActivity(addLotteryPurchase);
+            }
+        });
+
+        /**
+         *
+         *  ListView
+         *
+         */
+        customListView = new CustomListViewPurchase(this, 0, models);
+        mSwipPurchase.setAdapter(customListView);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                /*SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Open");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);*/
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(150);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        mSwipPurchase.setMenuCreator(creator);
+
+        mSwipPurchase.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index){
+                    case 0:
+                        int count = 0;
+                        for(PurchaseModel model:purchaseHelper.getAllLotteryPurchase()){
+                            if(count == position){
+                                models.remove(position);
+                                purchaseHelper.deleteLottery(model.getId());
+                            }
+                            count++;
+                            Toast.makeText(getApplication(), R.string.deleted, Toast.LENGTH_SHORT).show();
+                            querySQL();
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void querySQL(){
+        models = purchaseHelper.getAllLotteryPurchase();
+        customListView = new CustomListViewPurchase(this, 0, models);
+        mSwipPurchase.setAdapter(customListView);
+    }
+
+    //Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_stat,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_stat:
+                Intent a = new Intent(this, PurchaseStaticActivity.class);
+                startActivity(a);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
